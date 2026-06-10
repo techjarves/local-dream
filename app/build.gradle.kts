d542a3e3365176455b45a7f1ff5e540318322da4
xororz <xcandlrr@gmail.com>
2026-06-10 20:18:50 +0800

    v2.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         minSdk = 28
 //        minSdk = 31
         targetSdk = 36
-        versionCode = 70
-        versionName = "2.6.3"
+        versionCode = 71
+        versionName = "2.6.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
